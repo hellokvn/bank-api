bbeb85f43fdb79ea971d8391a21413ac65cf5d03
Kevin Vogel <hellokevinvogel@gmail.com>
2023-05-11 23:02:58 +0700

    chore(gateway): update bootstrap

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,11 +1,24 @@
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config: ConfigService = app.get(ConfigService);
+  const logger: Logger = new Logger();
+  const port: number = config.get('PORT');
 
-  app.enableVersioning();
-
-  await app.listen(3000);
+  await app.listen(port, () => {
+    logger.log(`[NOD] ${process.version}`);
+    logger.log(`[ENV] ${process.env.NODE_ENV}`);
+    logger.log(`[DKR] ${process.env.IS_DOCKER ? true : false}`);
+    logger.log(`[URL] http://localhost:${port}`);
+  });
 }
+
+function configure(app: INestApplication): void {
+  app.enableVersioning();
+}
+
 bootstrap();
